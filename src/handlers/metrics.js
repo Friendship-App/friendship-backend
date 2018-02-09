2f8abdb08febba13b5c2699c40c9d53ce8f6f0e9
minhhn <minh.hn2016@gmail.com>
2018-02-09 15:20:54 +0200

    added models&handlers to count lastActive from users, insert that into metrics_active_users

--- src/handlers/metrics.js
+++ src/handlers/metrics.js
@@ -9,6 +9,7 @@ import {
   dbGetRegisteredUsers,
   dbGetRegisteredUser,
   dbCreateRegisteredUser,
+  dbCreateActiveUsersCount,
 } from '../models/metrics';
 
 export const getNbMatchesMessaging = (request, reply) => {
@@ -23,10 +24,6 @@ export const getNbMessages = (request, reply) => {
   dbGetNbMessages().then(reply);
 };
 
-export const getNbActiveUsers = (request, reply) => {
-  dbGetNbActiveUsers().then(reply);
-};
-
 // danni
 export const getNbRegisteredUsers = (request, reply) => {
   dbGetRegisteredUsers().then(reply);
@@ -38,6 +35,16 @@ export const getRegisteredUsers = (request, reply) =>
 
 export const createRegisteredUser = (request, reply) =>
   dbCreateRegisteredUser({
+    ...request.payload,
+    users_count: request.payload.users_count,
+    timestamp: moment(),
+  }).then(reply);
+
+export const getNbActiveUsers = (request, reply) =>
+  dbGetNbActiveUsers().then(reply);
+
+export const createActiveUsersCount = (request, reply) =>
+  dbCreateActiveUsersCount({
     ...request.payload,
     users_count: request.payload.users_count,
     timestamp: moment(),
--- src/models/metrics.js
+++ src/models/metrics.js
@@ -2,6 +2,8 @@ import knex from '../utils/db';
 
 const registeredUsersFields = ['id', 'users_count', 'timestamp'];
 
+const countActiveUsersFields = ['id', 'users_count', 'timestamp'];
+
 export const dbGetNbMatchesMessaging = () => {
   return -1;
 };
@@ -14,9 +16,10 @@ export const dbGetNbMessages = () => {
   return -1;
 };
 
-export const dbGetNbActiveUsers = () => {
-  return -1;
-};
+
+export const dbUserLastActive = userId => knex('users')
+    .where({ id: userId })
+    .update({ lastActive: new Date() });
 
 export const dbGetRegisteredUsers = () =>
   knex('metrics_users_registered')
@@ -33,13 +36,34 @@ export const dbCreateRegisteredUser = ({ ...fields }) =>
     const registeredUser = await trx('metrics_users_registered')
       .insert(fields)
       .returning('*')
-      .then(results => results);
+      .then(results => results[0]);
 
     return registeredUser;
   });
 
-export const dbUserLastActive = userId =>
-  knex('users')
-  .where({ id: userId })
-    .update({ lastActive: new Date() });
+// count lastActive from users table, insert the result into metrics_active_users.users_count
+export const dbCreateActiveUsersCount = () => {
+  const lastActiveUsers = knex('users')
+    .count('lastActive');
+
+  knex.transaction(async (trx) => {
+    const activeUsersList = await trx('metrics_active_users')
+      .insert({ users_count: lastActiveUsers })
+      .returning('*')
+      .then(results => results[0]);
+
+    return activeUsersList;
+  });
+
+  return lastActiveUsers;
+};
+
+export const dbGetNbActiveUsers = () =>
+  knex('metrics_active_users')
+    .select(countActiveUsersFields);
+
+export const dbGetActiveUsersCount = id =>
+  knex('metrics_active_users')
+    .first()
+    .where({ id });
 

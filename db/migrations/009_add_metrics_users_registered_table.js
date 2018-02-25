42daeb9493a9cd0e42a15a06166c88289ba66ef6
Ha Van <vannunganha@gmail.com>
2018-02-25 19:52:56 +0200

    Remove metrics_users_registered in seed-dev

--- db/migrations/009_add_metrics_users_registered_table.js
+++ db/migrations/009_add_metrics_users_registered_table.js
@@ -2,7 +2,7 @@ exports.up = knex =>
     knex.schema
         .createTableIfNotExists('metrics_users_registered', (table) => {
             table.increments('id').primary();
-            table.integer('users_count');
+            table.integer('users_count').default(0);
             table.timestamp('timestamp');
         });
 

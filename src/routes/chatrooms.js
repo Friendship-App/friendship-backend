fc56950c5c4bcbe34053999e65940af4cace4328
Emmanuel Pastor <emmanuel.pastor@futurice.com>
2018-03-13 08:34:31 +0200

    Clean chatroom

--- src/routes/chatrooms.js
+++ src/routes/chatrooms.js
@@ -1,54 +1,41 @@
 import { getAuthWithScope } from '../utils/auth';
 import {
-    getChatrooms,
-    getChatroom,
-    createChatroom,
-    getAllMsFromChatrooms,
-    getChatroomsByUserId,
+  getChatroom,
+  createChatroom,
+  getChatroomsByUserId,
 } from '../handlers/chatrooms';
-import {
-  createMessage,
-} from '../handlers/messages';
+import { createMessage } from '../handlers/messages';
 
 const chatrooms = [
-    // Get a list of all users. them user name 2 con , last message or all mss
-  {
-    method: 'GET',
-    path: '/chatrooms',
-    handler: getChatrooms,
-  },
-   // Get all ms with chatroom Id
+  // Get all ms with chatroom Id
   {
     method: 'GET',
     path: '/chatrooms/{chatroomId}',
+    config: getAuthWithScope('user'),
     handler: getChatroom,
   },
-  {
-    method: 'GET',
-    path: '/chatrooms/userid/{userId}',
-    handler: getChatroomsByUserId,
-  },
-  // get all ms from all chatrooms
-  {
-    method: 'GET',
-    path: '/chatrooms/messages',
-    handler: getAllMsFromChatrooms,
-  },
- // Get info about a specific user
-  {
-    method: 'POST',
-    path: '/chatrooms',
-    config: getAuthWithScope('user'),
-    handler: createChatroom,
-  },
-  // create post
+  // create a message
   {
     method: 'POST',
     path: '/chatrooms/{chatroomId}',
     config: getAuthWithScope('user'),
     handler: createMessage,
   },
+  // Get all the chatrooms for a specific user
+  {
+    method: 'GET',
+    path: '/chatrooms/userid/{userId}',
+    config: getAuthWithScope('user'),
+    handler: getChatroomsByUserId,
+  },
+  // Create a chatroom
+  {
+    method: 'POST',
+    path: '/chatrooms',
+    config: getAuthWithScope('user'),
+    handler: createChatroom,
+  },
 ];
 export default chatrooms;
-  // Here we register the routes
+
 export const routes = server => server.route(chatrooms);

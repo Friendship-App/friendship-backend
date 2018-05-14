0e256ee0b4984f88e2572797e2b90f5bb37ce375
chengjo0 <john.cheng@futurice.com>
2018-05-14 15:27:11 +0300

    Link to avatars updated to be the same everywhere

--- db/migrations/0017_alter_emoji_to_avatar.js
+++ db/migrations/0017_alter_emoji_to_avatar.js
@@ -2,7 +2,7 @@ exports.up = knex =>
   knex.schema.alterTable('users', (t) => {
     t.dropColumn('emoji');
     t.string('avatar').defaultTo('https://friendship-app.s3.amazonaws.com/avatars/avatar1.png')
-  }).then(() => knex('users').update('avatar', 'https://s3.eu-central-1.amazonaws.com/friendship-app/avatars/avatar1.png'));
+  }).then(() => knex('users').update('avatar', 'https://friendship-app.s3.amazonaws.com/avatars/avatar1.png'));
 
 exports.down = knex =>
   knex.schema.table
